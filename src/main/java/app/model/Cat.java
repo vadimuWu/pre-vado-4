6b04f770964b3977568b81ab07cc843dcb841148
vadimuWu <vadimka.zaletaev@mail.ru>
2022-05-05 16:38:48 +0300

    first commit

---
+++ src/main/java/app/model/Cat.java
@@ -0,0 +1,11 @@
+package app.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Cat extends Animal {
+    @Override
+    public String toString() {
+        return "I'm a Cat";
+    }
+}
